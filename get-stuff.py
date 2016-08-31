86bcbc23a23c9dcc595c9b3d5409eb8684fe5044
Chris Barrett <chris@codesaru.com>
2016-08-31 14:10:03 -0700

    Handle duplicate Changeset db-insert attempt.

--- get-stuff.py
+++ get-stuff.py
@@ -3,6 +3,7 @@
 import os
 import requests
 import sqlite3
+import xml.etree.ElementTree
 
 # our bbox
 # w 0.681152
@@ -44,10 +45,12 @@ osm_api_url = 'http://api.openstreetmap.org/api/0.6/'
 
 
 def prepare_db():
-    db_conn = sqlite3.connect(':memory:')
+    global db_conn, db_cursor
+    #db_conn = sqlite3.connect(':memory:')
+    db_conn = sqlite3.connect('data/sqlite.db')
     db_cursor = db_conn.cursor()
     db_cursor.execute('''
-        CREATE TABLE Changeset (
+        CREATE TABLE IF NOT EXISTS Changeset (
             changesetId integer not null primary key,
             user text not null,
             userId integer not null,
@@ -59,7 +62,7 @@ def prepare_db():
         )
     ''')
     db_cursor.execute('''
-        CREATE TABLE ChangesetTag (
+        CREATE TABLE IF NOT EXISTS ChangesetTag (
             changesetId integer not null
                 REFERENCES Changeset(changesetId),
             key         text not null,
@@ -67,7 +70,7 @@ def prepare_db():
         )
     ''')
     db_cursor.execute('''
-        CREATE TABLE Node (
+        CREATE TABLE IF NOT EXISTS Node (
             nodeId    integer not null primary key,
             lat       number  not null,
             lon       number  not null,
@@ -78,7 +81,7 @@ def prepare_db():
         )
     ''')
     db_cursor.execute('''
-        CREATE TABLE Way (
+        CREATE TABLE IF NOT EXISTS Way (
             wayId       integer not null primary key,
             timestamp   text    not null,
             changesetId integer not null
@@ -88,7 +91,7 @@ def prepare_db():
         )
     ''')
     db_cursor.execute('''
-        CREATE TABLE WayNode (
+        CREATE TABLE IF NOT EXISTS WayNode (
             wayId  integer not null
                 REFERENCES Way(wayId),
             nodeId integer not null
@@ -96,7 +99,7 @@ def prepare_db():
         )
     ''')
     db_cursor.execute('''
-        CREATE TABLE WayTag (
+        CREATE TABLE IF NOT EXISTS WayTag (
             wayId integer not null
                 REFERENCES Way(wayId),
             key   text not null,
@@ -129,10 +132,47 @@ def store_changeset_from_api(id):
     return
 
 
+def store_changeset_file_in_db(id):
+    osm_element = xml.etree.ElementTree.parse('data/changesets/{}.xml'.format(id)).getroot()
+    for changeset_element in osm_element:
+        sql_params = [
+            changeset_element.attrib['id'],
+            changeset_element.attrib['user'],
+            changeset_element.attrib['uid'],
+            changeset_element.attrib['created_at'],
+            changeset_element.attrib['min_lat'],
+            changeset_element.attrib['min_lon'],
+            changeset_element.attrib['max_lat'],
+            changeset_element.attrib['max_lon'],
+        ]
+        try:
+            db_cursor.execute(
+                '''
+                    INSERT OR ABORT INTO Changeset (changesetId, user, userId, createdAt, min_lat, min_lon, max_lat, max_lon)
+                    VALUES (?,?,?,?,?,?,?,?)
+                ''',
+                sql_params
+            )
+        except sqlite3.IntegrityError as e:
+            continue  # if changeset exists, skip inserting its tags
+        for tag_element in changeset_element:
+            sql_params = [
+                changeset_element.attrib['id'],
+                tag_element.attrib['k'],
+                tag_element.attrib['v'],
+            ]
+            db_cursor.execute(
+                'INSERT INTO ChangesetTag (changesetId, key, value) VALUES (?,?,?)',
+                sql_params
+            )
+    return
+
+
 def get_changeset_from_disk(id):
     xml_filepath = 'data/changesets/{}.xml'.format(id)
     if not os.path.isfile(xml_filepath):
         return None
+    return xml.etree.ElementTree.parse(xml_filepath).getroot()
     with open(xml_filepath) as f:
         return f.read()
 
@@ -154,4 +194,9 @@ changeset_url_format = 'http://api.openstreetmap.org/api/0.6/changeset/{}'  # (i
 prepare_db()
 get_stuff_in_area()
 store_changeset_from_api(41177103)
+store_changeset_file_in_db(41177103)
+print(db_conn.execute('select * from Changeset').fetchall())
+
+db_conn.commit()
+db_conn.close()
 
